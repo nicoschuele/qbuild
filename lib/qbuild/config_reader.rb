88c5a412c84f4a7ff04be09511b547d4f7e8e168
Nico Schuele -MB <nicoschuele@gmail.com>
2016-11-16 00:14:22 +0100

    Add a config reader for the unique snowflakes out there

---
+++ lib/qbuild/config_reader.rb
@@ -0,0 +1,18 @@
+require 'pad_utils'
+
+module ConfigReader
+  def self.options
+    opts = {}
+    unless PadUtils.file_exists?('.qbuild.json')
+      opts = { js_path: 'scripts', scss_path: 'stylesheets', css_path: 'stylesheets' }
+      return opts
+    end
+    conf = read_config
+    opts = { js_path: conf[:js_path], scss_path: conf[:scss_path], css_path: [:css_path] }
+    opts
+  end
+
+  def self.read_config
+    PadUtils.json_file_to_hash('.qbuild.json')
+  end
+end
