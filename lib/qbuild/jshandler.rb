8441644c1704a2f35c50fc0faa51a5a200ca830e
Nico Schuele -MB <nicoschuele@gmail.com>
2016-11-16 19:29:20 +0100

    Use the brand new json options

--- lib/qbuild/jshandler.rb
+++ lib/qbuild/jshandler.rb
@@ -2,18 +2,22 @@ require 'uglifier'
 require 'pad_utils'
 require_relative 'config_reader'
 
-module Jshandler
-  def self.minify_js
-    js_path = ConfigReader.options[:js_path]
-    Dir["#{js_path}/*.js"].each do |file|
-      unless file.include? '.min.js'
-        jsmin = Uglifier.new.compile(File.read(file))
-        File.write(name_min_file(file), jsmin)
+module Qbuild
+  module Jshandler
+    def self.minify_js
+      js_paths = Qbuild::Config.js_paths
+      js_paths.each do |js_path|
+        Dir["#{js_path}/*.js"].each do |file|
+          unless file.include? '.min.js'
+            jsmin = Uglifier.new.compile(File.read(file))
+            File.write(name_min_file(file), jsmin)
+          end
+        end
       end
     end
-  end
 
-  def self.name_min_file(file)
-    file.gsub(/(.*)(.js)(.*)/, '\1.min.js\3')
+    def self.name_min_file(file)
+      file.gsub(/(.*)(.js)(.*)/, '\1.min.js\3')
+    end
   end
 end
